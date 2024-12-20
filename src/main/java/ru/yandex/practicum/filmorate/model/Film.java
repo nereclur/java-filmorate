514dfd24a771d7db2684923db5e984b96c03d64a
dmitry <nereclur@yandex.ru>
2024-12-20 06:43:23 +0300

    Отчаяный коммит

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,13 +1,22 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Data;
+import lombok.*;
+
 import java.time.LocalDate;
 
 @Data
+@RequiredArgsConstructor
 public class Film {
     private int id;
     private String name;
     private String description;
     private LocalDate releaseDate;
     private int duration;
+
+    public Film(String name, String description, LocalDate releaseDate, int duration) {
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,13 +1,23 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 import java.time.LocalDate;
 
 @Data
+@RequiredArgsConstructor
 public class User {
     private int id;
     private String email;
     private String login;
     private String name;
     private LocalDate birthday;
+
+    public User(String email, String login, String name, LocalDate birthday) {
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
 }
